883744dce3b8191fdcc7621605df7feaa7d04976
wahyupurnama43 <wahyupurnama434@gmail.com>
2021-07-01 15:32:43 +0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,28 +13,28 @@ $(document).scroll(function () {
     if (widhtBody < 1024) {
         if (y > 350) {
             $("nav.menu").addClass("shActive");
-            $(".menu-toggle span#span1").addClass('bg-dark');
-            $(".menu-toggle span#span2").addClass('bg-dark');
-            $(".menu-toggle span#span3").addClass('bg-dark');
+            $(".menu-toggle span#span1").addClass("bg-dark");
+            $(".menu-toggle span#span2").addClass("bg-dark");
+            $(".menu-toggle span#span3").addClass("bg-dark");
         } else {
             $("nav.menu").removeClass("shActive");
-            $(".menu-toggle span#span1").removeClass('bg-dark');
-            $(".menu-toggle span#span2").removeClass('bg-dark');
-            $(".menu-toggle span#span3").removeClass('bg-dark');
+            $(".menu-toggle span#span1").removeClass("bg-dark");
+            $(".menu-toggle span#span2").removeClass("bg-dark");
+            $(".menu-toggle span#span3").removeClass("bg-dark");
         }
     } else {
         if (y > 700) {
             $("nav.menu").addClass("shActive");
             $("nav ul li a").addClass("text-black-primary");
-             $(".menu-toggle span#span1").addClass('bg-dark');
-            $(".menu-toggle span#span2").addClass('bg-dark');
-            $(".menu-toggle span#span3").addClass('bg-dark');
+            $(".menu-toggle span#span1").addClass("bg-dark");
+            $(".menu-toggle span#span2").addClass("bg-dark");
+            $(".menu-toggle span#span3").addClass("bg-dark");
         } else {
             $("nav.menu").removeClass("shActive");
             $("nav ul li a").removeClass("text-black-primary");
-             $(".menu-toggle span#span1").removeClass('bg-dark');
-            $(".menu-toggle span#span2").removeClass('bg-dark');
-            $(".menu-toggle span#span3").removeClass('bg-dark');
+            $(".menu-toggle span#span1").removeClass("bg-dark");
+            $(".menu-toggle span#span2").removeClass("bg-dark");
+            $(".menu-toggle span#span3").removeClass("bg-dark");
         }
     }
 });
